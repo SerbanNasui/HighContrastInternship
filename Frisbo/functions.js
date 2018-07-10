e6d947820f0d77282bbb91c5a90880d3a50a7339
DESKTOP-0K70U3P\serba <serban.nasui@gmail.com>
2018-07-10 15:49:04 +0300

    finish footer && add goToTop button

--- Frisbo/functions.js
+++ Frisbo/functions.js
@@ -8,3 +8,21 @@ document.getElementById("mySidenav").style.width = "0";
 document.body.style.backgroundColor = "white";
 }
 
+
+
+//goToTop
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+
+
+function topFunction() {
+    document.body.scrollTop = 0; 
+    document.documentElement.scrollTop = 0; 
+}
