4480b94bcffa3ad2f0f1b08b094ff041f8f00d65
DESKTOP-0K70U3P\serba <serban.nasui@gmail.com>
2018-07-17 16:57:41 +0300

    navbar and map responsive

--- Frisbo/functions.js
+++ Frisbo/functions.js
@@ -1,11 +1,11 @@
 function openNav() {
     document.getElementById("mySidenav").style.width = "350px";
-    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+    document.getElementsByClassName('menu-overlay')[0].style.display = 'block';
 }
 
 function closeNav() {
 document.getElementById("mySidenav").style.width = "0";
-document.body.style.backgroundColor = "white";
+document.getElementsByClassName('menu-overlay')[0].style.display = 'none';
 }
 
 
