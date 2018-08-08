9d1e9495ac153161af8bdac784a0b333321c7315
DESKTOP-0K70U3P\serba <serban.nasui@gmail.com>
2018-08-08 14:29:25 +0300

    arrays

--- php/page.php
+++ php/page.php
@@ -1,22 +1,13 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <style>
-        header {
-            background: #e3e3e3;
-            padding: 2em;
-            text-align: center;
-        }
-    </style>
-</head>
-<body>
-    <header>
-        <h1><?php echo "Hello World"?></h1>
-    </header>
-</body>
-</html>
+<?php
+
+$greeting = "Hello world!";
+
+$names =[
+    'Serban',
+    'Larisa',
+    'pwp'
+];
+
+
+
+require 'page.view.php';
