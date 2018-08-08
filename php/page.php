65654485b2cf3f979b39003455115cafebf0fedd
DESKTOP-0K70U3P\serba <serban.nasui@gmail.com>
2018-08-08 15:09:41 +0300

    end ep7

--- php/page.php
+++ php/page.php
@@ -8,6 +8,18 @@ $names =[
     'pwp'
 ];
 
+$person = [
+    'age' => 20,
+    'hair' => 'brown',
+    'career' => 'web developer'
+];
 
+$person ['name'] = 'Serban';
+
+
+//echo '<pre>';
+//var_dump($person);
+//echo '</pre>';
+//echo $person['age'];
 
 require 'page.view.php';
--- php/page.view.php
+++ php/page.view.php
@@ -29,5 +29,12 @@
     ?>
 </ul>
 
+<ul>
+    <?php foreach ($person as $key =>$feature ):?>
+        <li><strong><?= $key;?></strong> <?=$feature;?></li>
+    <?php endforeach;?>
+</ul>
+
+
 </body>
 </html>
